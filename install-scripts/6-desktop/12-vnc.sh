f90c6c71258aea348d2f0a30ca6028d19f94a67c
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2023-06-25 15:13:14 -0400

    Update 12-vnc.sh

--- install-scripts/6-desktop/12-vnc.sh
+++ install-scripts/6-desktop/12-vnc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
-wget https://downloads.realvnc.com/download/file/vnc.files/VNC-Server-7.5.1-Linux-ARM64.deb
-dpkg -i VNC-Server-*-ARM64.deb
-rm VNC-Server-*.deb
+#wget https://downloads.realvnc.com/download/file/vnc.files/VNC-Server-7.5.1-Linux-ARM64.deb
+#dpkg -i VNC-Server-*-ARM64.deb
+#rm VNC-Server-*.deb
 
-systemctl enable vncserver-x11-serviced.service
+#systemctl enable vncserver-x11-serviced.service
