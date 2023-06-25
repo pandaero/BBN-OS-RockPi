b474593a01e7cd4aa33a6a9b2db12215317521e0
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2023-06-25 14:02:17 -0400

    Update 00-desktopEnvironment.sh

--- install-scripts/6-desktop/00-desktopEnvironment.sh
+++ install-scripts/6-desktop/00-desktopEnvironment.sh
@@ -74,7 +74,8 @@ else
 fi
 
 # Adobe Flash Player. Copyright 1996-2015. Adobe Systems Incorporated. All Rights Reserved.
-DEBIAN_FRONTEND=noniterractive apt-get -o Dpkg::Options:=="--force-confnew" -q -y install rpi-chromium-mods
+# TODO:
+#DEBIAN_FRONTEND=noniterractive apt-get -o Dpkg::Options:=="--force-confnew" -q -y install rpi-chromium-mods
 
 apt-get install -y -q lxterminal gpsbabel file-roller lxtask thunar
 
